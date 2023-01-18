8cbea6e657d010ba0c1702ecfea3c28a140e1f90
Celeste McCall <celestemccall93@gmail.com>
2023-01-18 17:29:44 -0600

    Update to questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, What is your first name?");
+            Console.WriteLine("Hello there! What is your first name?");
             var userName = Console.ReadLine();
 
             Console.WriteLine($"Nice to meet you {userName}! What is your favorite color?");
@@ -15,6 +15,8 @@
 
             Console.WriteLine($"That is so awesome {userName}, one last question, what is your favorite band?");
             var band = Console.ReadLine();
+
+            Console.WriteLine($"It has been wonderful getting to know you {userName}, talk to you soon!");
         }
     }
 }
