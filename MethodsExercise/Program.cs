b6372f3bfbbc65e94ec378fcc44c5c10933db58d
Celeste McCall <celestemccall93@gmail.com>
2023-01-18 17:44:40 -0600

    Created a method

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,22 +1,29 @@
 ﻿namespace MethodsExercise
 {
-    public class Program
+    class Program
     {
-        static void Main(string[] args)
+        public static void HelloThere()
         {
             Console.WriteLine("Hello there! What is your first name?");
             var userName = Console.ReadLine();
 
-            Console.WriteLine($"Nice to meet you {userName}! What is your favorite color?");
+        Console.WriteLine($"Nice to meet you {userName}! What is your favorite color?");
             var color = Console.ReadLine();
 
-            Console.WriteLine($"{color} is my favorite color too! What's your favorite animal?");
+        Console.WriteLine($"{color} is my favorite color too! What's your favorite animal?");
             var animal = Console.ReadLine();
 
-            Console.WriteLine($"That is so awesome {userName}, one last question, what is your favorite band?");
+        Console.WriteLine($"That is so awesome {userName}, one last question, what is your favorite band?");
             var band = Console.ReadLine();
 
-            Console.WriteLine($"It has been wonderful getting to know you {userName}, talk to you soon!");
+        Console.WriteLine($"It has been wonderful getting to know you {userName}, talk to you soon!");
+        } 
+
+    static void Main(string[] args)
+        {
+            HelloThere();
+
+           
         }
     }
 }
