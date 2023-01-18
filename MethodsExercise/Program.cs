1b1536f2774fd242bb7b9ff984ecc4fa88cb5b45
Celeste McCall <celestemccall93@gmail.com>
2023-01-18 17:24:20 -0600

    began user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Nice to meet you {userName}! What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is my favorite color too! What's your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"That is so awesome {userName}, one last question, what is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
